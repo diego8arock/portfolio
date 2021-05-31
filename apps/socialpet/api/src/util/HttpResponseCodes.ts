82d92b78bada5f314c09eda7dac0a58c9791712a
diego8arock <diegoa2000alt@gmail.com>
2021-05-31 15:43:22 -0500

    add http response code class

---
+++ apps/socialpet/api/src/util/HttpResponseCodes.ts
@@ -0,0 +1,8 @@
+export class HttpRequestCodes {
+  //4XX
+  public static readonly BAD_REQUEST = 400;
+  public static readonly RESOURCE_NOT_FOUND = 404;
+
+  //500
+  public static readonly SERVER_ERROR = 500;
+}
