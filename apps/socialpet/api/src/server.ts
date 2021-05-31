3984bd09e65c163533e302bc4f8a99f01416a0ab
diego8arock <diegoa2000alt@gmail.com>
2021-05-31 15:44:52 -0500

    add db connection, user route, encode and dotenv

--- apps/socialpet/api/src/server.ts
+++ apps/socialpet/api/src/server.ts
@@ -1,8 +1,13 @@
 import express, { Request, Response } from "express";
+import { createConnection } from "typeorm";
+import { UsersController } from "./controller/user.controller";
+import dotenv from "dotenv";
 
 class Server {
   private app: express.Application;
+  private userController!: UsersController;
   constructor() {
+    dotenv.config();
     this.app = express();
     this.config();
     this.routes();
@@ -10,9 +15,30 @@ class Server {
 
   public config() {
     this.app.set("port", process.env.PORT || 3000);
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
   }
 
-  public routes() {
+  public async routes() {
+    await createConnection({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || "5432"),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: ["build/database/entities/**/*.js"],
+      synchronize: true,
+      logging: process.env.DB_LOGGING?.toLowerCase() == "true",
+      name: "default",
+      ssl:
+        process.env.NODE_ENV == "development"
+          ? false
+          : { rejectUnauthorized: false },
+    });
+
+    this.userController = new UsersController();
+    this.app.use("/api/users/", this.userController.router);
     this.app.get("/", (req: Request, res: Response) => {
       res.send("SocialPet API running...");
     });
@@ -20,7 +46,11 @@ class Server {
 
   public start() {
     this.app.listen(this.app.get("port"), () => {
-      console.log(`Server is listening ${this.app.get("port")} port.`);
+      console.log(
+        `Server is listening ${this.app.get("port")} port on ${
+          process.env.NODE_ENV
+        } environment.`
+      );
     });
   }
 }
