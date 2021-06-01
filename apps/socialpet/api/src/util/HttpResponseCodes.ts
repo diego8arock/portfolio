64ef9be5992c8f2c89351d49ec74ebabef7c8b92
diego8arock <diegoa2000alt@gmail.com>
2021-06-01 15:19:02 -0500

    add http codes

--- apps/socialpet/api/src/util/HttpResponseCodes.ts
+++ apps/socialpet/api/src/util/HttpResponseCodes.ts
@@ -1,7 +1,13 @@
 export class HttpRequestCodes {
+  //2XX
+  public static readonly OK = 200;
+  public static readonly CREATED = 201;
+
   //4XX
   public static readonly BAD_REQUEST = 400;
+  public static readonly UNAUTHORIZED = 401;
   public static readonly RESOURCE_NOT_FOUND = 404;
+  public static readonly UNPROCESSABLE_ENTITY = 422;
 
   //500
   public static readonly SERVER_ERROR = 500;
