0f11d77ab6018394ef89ca4159ce333c1a8780bc
diego8arock <diegoa2000alt@gmail.com>
2021-06-03 14:17:13 -0500

    add more properties

--- apps/socialpet/api/src/util/Utils.ts
+++ apps/socialpet/api/src/util/Utils.ts
@@ -4,12 +4,16 @@ export class Utils {
     const userPassword = credentialsAndConnection[0].split(":");
     const hostPortDbName = credentialsAndConnection[1].split("/");
     const hostPort = hostPortDbName[0].split(":");
+    const logging = process.env.DB_LOGGING?.toLowerCase() == "true";
+    const ssl = process.env.NODE_ENV != "production" ? false : { rejectUnauthorized: false };
     const dbconfig = {
       host: hostPort[0],
       port: parseInt(hostPort[1]),
       username: userPassword[0],
       password: userPassword[1],
       database: hostPortDbName[1],
+      logging: logging,
+      ssl: ssl,
     };
 
     return dbconfig;
