0a8a372a3c340ff8870335172b62f7e96d47a55e
diego8arock <diegoa2000alt@gmail.com>
2021-06-03 14:03:22 -0500

    add new tests and config

---
+++ apps/socialpet/api/test/0.before.spec.ts
@@ -0,0 +1,19 @@
+import chai from "chai";
+import chaiHttp from "chai-http";
+import { UserRepository } from "../src/database/repository/UserRepository";
+
+import { getConnection } from "typeorm";
+import app from "../src/server";
+
+let requester: ChaiHttp.Agent;
+
+before((done) => {
+  requester = chai.request(app).keepOpen();
+  setTimeout(async () => {
+    console.log("Cleaning DB");
+    const connection = getConnection("default");
+    await connection.getCustomRepository(UserRepository).clear();
+    console.log("DB Clean");
+    done();
+  }, 2000);
+});
--- apps/socialpet/api/test/1.user.spec.ts
+++ apps/socialpet/api/test/1.user.spec.ts
@@ -6,26 +6,27 @@ import { userMain } from "./data";
 chai.should();
 chai.use(chaiHttp);
 
-let requester: ChaiHttp.Agent;
-
-before((done) => {
-  requester = chai.request(app).keepOpen();
-  setTimeout(() => {
-    done();
-  }, 2000);
-});
-
-describe("/GET ", () => {
+describe("/GET app runing", () => {
   it("it should return 200", async () => {
     const res = await chai.request(app).get("/");
     res.should.have.status(200);
   });
 });
 
-describe("/POST users", () => {
+describe("/POST users new sing up", () => {
   it("it should return token", async () => {
     const res = await chai.request(app).post("/api/users").send(userMain);
     console.log(res.body);
-    res.should.have.status(200);
+    res.should.have.status(201);
+    res.body.should.have.property("token");
+  });
+});
+
+describe("/POST users old sing up", () => {
+  it("it should return error 400", async () => {
+    const res = await chai.request(app).post("/api/users").send(userMain);
+    console.log(res.body);
+    res.should.have.status(400);
+    res.body.should.have.property("errors");
   });
 });
