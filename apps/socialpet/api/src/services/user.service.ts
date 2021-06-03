46be04c3ef3b0cd2fef9c70266b57d07b19dcc18
diego8arock <diegoa2000alt@gmail.com>
2021-06-03 14:04:46 -0500

    code refractor and add find user by email

--- apps/socialpet/api/src/services/user.service.ts
+++ apps/socialpet/api/src/services/user.service.ts
@@ -3,24 +3,21 @@ import { UserEntity } from "../database/entities/UserEntity";
 import { UserRepository } from "../database/repository/UserRepository";
 import { HttpRequestCodes } from "../util/HttpResponseCodes";
 import { ServiceResponse } from "../util/responses/serviceResponses/ServiceResponse";
-import { ServiceResponseFactory } from "../util/responses/serviceResponses/ServiceResponseFactory";
+import { ServiceResponseFactory as srf } from "../util/responses/serviceResponses/ServiceResponseFactory";
+import { UserServiceResponse } from "../util/responses/serviceResponses/UserServiceResponse";
 
 export class UserService {
   private userRepository: UserRepository;
   constructor() {
-    this.userRepository =
-      getConnection("default").getCustomRepository(UserRepository);
+    this.userRepository = getConnection("default").getCustomRepository(UserRepository);
   }
 
   public isEmailUnique = async (email: string): Promise<ServiceResponse> => {
-    let response = ServiceResponseFactory.createDefaultServiceResponse();
+    let response = srf.createDefaultServiceResponse();
     try {
       const userCount = await this.userRepository.count({ email: email });
       if (userCount != 0) {
-        response.setError(
-          `User with email ${email} already exists`,
-          HttpRequestCodes.BAD_REQUEST
-        );
+        response.setError(`User with email ${email} already exists`, HttpRequestCodes.BAD_REQUEST);
       }
     } catch (error) {
       console.log(error);
@@ -29,8 +26,20 @@ export class UserService {
     return response;
   };
 
+  public findByEmail = async (email: string): Promise<UserServiceResponse> => {
+    const response = srf.createDefaultUserServiceResponse();
+    try {
+      const user = await this.userRepository.findOne({ email: email });
+      if (user) response.user = user;
+      else response.setError(`User with email ${email} was not found`, HttpRequestCodes.RESOURCE_NOT_FOUND);
+    } catch (error) {
+      response.setError(error, HttpRequestCodes.SERVER_ERROR);
+    }
+    return response;
+  };
+
   public save = async (user: UserEntity): Promise<ServiceResponse> => {
-    let response = ServiceResponseFactory.createDefaultServiceResponse();
+    let response = srf.createDefaultServiceResponse();
     try {
       await this.userRepository.save(user);
     } catch (error) {
