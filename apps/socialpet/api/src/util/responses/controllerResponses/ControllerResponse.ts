8207dd9ec6eaf99ea60413bb98ebb46a15146161
diego8arock <diegoa2000alt@gmail.com>
2021-06-03 14:04:20 -0500

    add create basic error response

--- apps/socialpet/api/src/util/responses/controllerResponses/ControllerResponse.ts
+++ apps/socialpet/api/src/util/responses/controllerResponses/ControllerResponse.ts
@@ -2,12 +2,17 @@ import { Response } from "express";
 import { ServiceResponse } from "../serviceResponses/ServiceResponse";
 
 export class ControllerResponse {
-  public static createErrorResponse(
-    res: Response,
-    serviceResponse: ServiceResponse
-  ) {
-    return res
-      .status(serviceResponse.statusCode)
-      .json({ errors: serviceResponse.errors });
+  public static createServiceErrorResponse(res: Response, serviceResponse: ServiceResponse): Response {
+    return res.status(serviceResponse.statusCode).json({ errors: serviceResponse.errors });
+  }
+
+  public static createBasicErrorResponse(res: Response, errors: string | any[], statusCode: number): Response {
+    if (typeof errors === "string") {
+      const temp = [];
+      temp.push(errors);
+      return res.status(statusCode).json({ errors: temp });
+    } else {
+      return res.status(statusCode).json({ errors: errors });
+    }
   }
 }
