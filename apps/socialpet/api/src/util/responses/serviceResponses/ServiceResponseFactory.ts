c47f26b27ec8a88abe770468a8ef8e542ef8a99e
diego8arock <diegoa2000alt@gmail.com>
2021-06-08 13:49:16 -0500

    add creator for request response

--- apps/socialpet/api/src/util/responses/serviceResponses/ServiceResponseFactory.ts
+++ apps/socialpet/api/src/util/responses/serviceResponses/ServiceResponseFactory.ts
@@ -1,19 +1,25 @@
 import { PostEntity } from "../../../database/entities/PostEntity";
 import { UserEntity } from "../../../database/entities/UserEntity";
+import { HttpRequestCodes } from "../../HttpResponseCodes";
 import { PostServiceResponse } from "./PostServiceResponse";
+import { RequestServiceResponse } from "./RequestServiceResponse";
 import { ServiceResponse } from "./ServiceResponse";
 import { UserServiceResponse } from "./UserServiceResponse";
 
 export class ServiceResponseFactory {
   public static createDefaultServiceResponse() {
-    return new ServiceResponse(true, [], 200);
+    return new ServiceResponse(true, [], HttpRequestCodes.OK);
   }
 
   public static createDefaultUserServiceResponse() {
-    return new UserServiceResponse(true, [], 200, new UserEntity());
+    return new UserServiceResponse(true, [], HttpRequestCodes.OK, new UserEntity());
   }
 
   public static createDefaultPostServiceResponse() {
-    return new PostServiceResponse(true, [], 200, new PostEntity());
+    return new PostServiceResponse(true, [], HttpRequestCodes.OK, new PostEntity());
+  }
+
+  public static createDefaultRequestServiceResponse() {
+    return new RequestServiceResponse(true, [], HttpRequestCodes.OK, []);
   }
 }
