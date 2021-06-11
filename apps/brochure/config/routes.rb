162866c51f09d1b89820d1bd016ff9b0399269c6
diego8arock <diegoa2000alt@gmail.com>
2021-06-11 14:27:35 -0500

    remove old routes

--- apps/brochure/config/routes.rb
+++ apps/brochure/config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resources :likes
-  resources :news
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     root 'welcome#index'
 end
